d9b45221ff6be3fe1cb83e149753165767a385e9
David Wilson <david.wilson@govsacademy.org>
2016-12-03 05:14:11 -0500

    update

--- parse.c
+++ parse.c
@@ -1,13 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
 #include "parse.h"
 
 typedef struct ParseState ParseState;
 
 struct ParseState {
-	TreeNode* root_node;
-	TreeNode* root_block;
+	TokenList* tokens;
+	TreeNode* root_node; /* type == NODE_BLOCK */
+	TreeNode* current_block;
 };
+
+/* parse functions */
+static void parse_if(ParseState*);
+
+/* misc */
+static void parse_die(ParseState*, const char*, ...);
+static void assert_operator(ParseState*, unsigned int);
+static void eat_operator(ParseState*, unsigned int);
+
+static void
+parse_die(ParseState* P, const char* message, ...) {
+	va_list args;
+	va_start(args, message);	
+	printf("\n\n** SPYRE PARSE ERROR **\n\tmessage: ");
+	vprintf(message, args);
+	if (P->tokens) {
+		printf("\n\tline: %d\n", P->tokens->token->line);
+	}
+	printf("\n\n\n");
+	va_end(args);
+	exit(1);
+}
+
+static void
+assert_operator(ParseState* P, unsigned int type) {
+	if (!P->tokens || P->tokens->token->type != TOK_OPERATOR || P->tokens->token->oval != type) {
+		parse_die(P, "expected something else lol");	
+	}
+}
+
+/* this is just assert_operator and advance */
+static void
+eat_operator(ParseState* P, unsigned int type) {
+	assert_operator(P, type);
+	P->tokens = P->tokens->next;
+}
+
+static void parse_if(ParseState* P) {
+	/* starts on token IF */
+	P->tokens = P->tokens->next; /* skip IF */
+	eat_operator(P, '(');
+}
 
 TreeNode*
 generate_syntax_tree(TokenList* tokens) {
 
+	ParseState P;
+	P.tokens = tokens;
+	P.root_node = malloc(sizeof(TreeNode));
+	P.root_node->type = NODE_BLOCK;
+	P.root_node->blockval = malloc(sizeof(TreeBlock));
+	P.root_node->blockval->child = NULL;
+	P.current_block = P.root_node;
+
+	while (P.tokens) {
+		if (P.tokens->token->type == TOK_IDENTIFIER) {
+			const char* onword = P.tokens->token->sval;
+			if (!strcmp(onword, "if")) {
+				parse_if(&P);
+			}	
+		} else {
+			P.tokens = P.tokens->next;
+		}
+	}
 }
--- parse.h
+++ parse.h
@@ -4,16 +4,39 @@
 #include "lex.h"
 
 typedef struct TreeNode TreeNode;
+typedef struct TreeIf TreeIf;
+typedef struct TreeWhile TreeWhile;
+typedef struct TreeFor TreeFor;
+typedef struct TreeFunctionImpl TreeFunctionImpl;
+typedef struct TreeBreak TreeBreak;
+typedef struct TreeContinue TreeContinue;
+typedef struct TreeReturn TreeReturn;
+typedef struct TreeBlock TreeBlock;
+
 typedef struct ExpNode ExpNode;
 typedef struct BinaryOp BinaryOp;
 typedef struct UnaryOp UnaryOp;
+
 typedef enum ExpNodeType ExpNodeType;
+typedef enum TreeNodeType TreeNodeType;
 
 enum ExpNodeType {
-	NODE_NONODE = 0,
-	NODE_UNARY = 1,
-	NODE_BINARY = 2,
-	NODE_CAST = 3
+	EXP_NOEXP = 0,
+	EXP_UNARY = 1,
+	EXP_BINARY = 2,
+	EXP_CAST = 3
+};
+
+enum TreeNodeType {
+	NODE_IF = 1,
+	NODE_FOR = 2,
+	NODE_WHILE = 3,
+	NODE_FUNC_IMPL = 4,
+	NODE_STATEMENT = 5,
+	NODE_BREAK = 6,
+	NODE_CONTINUE = 7,
+	NODE_RETURN = 8,
+	NODE_BLOCK = 9
 };
 
 struct BinaryOp {
@@ -35,8 +58,17 @@ struct ExpNode {
 	};
 };
 
+struct TreeBlock {
+	TreeNode* child;
+};
+
 struct TreeNode {
-	
+	TreeNode* parent;
+	TreeNode* next;
+	TreeNodeType type;
+	union {
+		TreeBlock* blockval;	
+	};
 };
 
 TreeNode* generate_syntax_tree(TokenList*);
